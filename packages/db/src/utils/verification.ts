482b38ce4908890888830ee8dfa18ca14106d507
Eugene Matsumura <eugene.bb1@gmail.com>
2023-02-02 15:44:31 +0100

    feat: update verification code

--- packages/db/src/utils/verification.ts
+++ packages/db/src/utils/verification.ts
@@ -1,21 +1,29 @@
 import { Redis } from "ioredis";
 import { v4 } from "uuid";
+import { User } from "@prisma/client";
+import { upsertUser } from "./user";
 
 const redis = new Redis();
 
-export const createVerification = async (userId: string) => {
+type UserData = Omit<Omit<User, "createdAt">, "subscribed">;
+
+export const createVerification = async (user: UserData) => {
   const token = v4();
 
-  await redis.set(token, userId, "EX", 1000 * 60 * 60 * 24); // 1 day
+  await redis.set(token, JSON.stringify(user), "EX", 1000 * 60 * 60 * 24); // 1 day
   return token;
 };
 
 export const checkVerification = async (token: string) => {
-  const userId = await redis.get(token);
+  const userStr = await redis.get(token);
 
-  if (!userId) {
+  if (!userStr) {
     return false;
   }
+
+  const user = JSON.parse(userStr) as UserData;
+  await redis.del(token);
+  await upsertUser(user);
 
   return true;
 };
