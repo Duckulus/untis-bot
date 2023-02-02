ad5f415c6b3bb75ad4149c9dc4067df90d50677e
Eugene Matsumura <eugene.bb1@gmail.com>
2023-02-02 16:08:45 +0100

    feat: handle invalid input

--- apps/web/src/pages/api/verify/[token].tsx
+++ apps/web/src/pages/api/verify/[token].tsx
@@ -2,16 +2,21 @@ import { checkVerification } from "db";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { z } from "zod";
 
-type ResData = { found: boolean };
+type ResData = { found?: boolean; error?: string };
 
 const querySchema = z.object({
   token: z.string(),
 });
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<ResData>) => {
-  const { token } = querySchema.parse(req.query);
-  const found = await checkVerification(token);
+  const queryData = querySchema.safeParse(req.query);
+  if (!queryData.success) {
+    res.status(400).json({ error: "Failed to validate input" });
+    return;
+  }
 
+  const { token } = queryData.data;
+  const found = await checkVerification(token);
   res.status(200).json({ found });
 };
 
--- apps/web/src/pages/api/verify/index.ts
+++ apps/web/src/pages/api/verify/index.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { createVerification, UserData } from "db";
 import { z } from "zod";
 
-type ResData = { token: string };
+type ResData = { token?: string; error?: string };
 
 const userSchema = z.object({
   number: z.string(),
@@ -13,8 +13,13 @@ const userSchema = z.object({
 });
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<ResData>) => {
-  const user: UserData = userSchema.parse(req.body);
+  const userParse = userSchema.safeParse(req.body);
+  if (!userParse.success) {
+    res.status(400).json({ error: "Failed to validate input" });
+    return;
+  }
 
+  const user = userParse.data;
   const token = await createVerification(user);
   res.status(200).json({ token });
 };
