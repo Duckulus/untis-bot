625c9e643c7222c5056f9263b9fecb93d5cba26c
Amin <amin.haddou@frg.wwschool.de>
2023-02-04 11:11:25 +0100

    add dailytime command

---
+++ packages/whatsapp/src/commands/dailytime.ts
@@ -0,0 +1,44 @@
+import { Command, COMMAND_PREFIX } from "../core/command";
+import { upsertUser } from "@untis-bot/db";
+import { scheduleDailyTask } from "../core/daily";
+import { whatsAppClient } from "../core/client";
+
+Command.create({
+  name: "dailytime",
+  execute: async (msg, args, user) => {
+    if (!user) return;
+    const timeString = args[0];
+    if (!timeString) {
+      await msg.reply(`Current time: ${user.hours
+        .toString()
+        .padStart(2, "0")}:${user.minutes
+        .toString()
+        .padStart(2, "0")}\n\nUse ${COMMAND_PREFIX}dailytime <time> to change.
+      `);
+      return;
+    } else {
+      const hoursAndMinutes = timeString.split(":");
+      const hours = parseInt(timeString.split(":")[0]);
+      const minutes = parseInt(timeString.split(":")[1]);
+      if (
+        hoursAndMinutes.length != 2 ||
+        isNaN(hours) ||
+        isNaN(minutes) ||
+        hours < 0 ||
+        hours > 24 ||
+        minutes < 0 ||
+        minutes > 60
+      ) {
+        await msg.reply("Invalid time String. Format: hours:minutes");
+        return;
+      }
+      await upsertUser({
+        number: user.number,
+        hours: hours,
+        minutes: minutes,
+      });
+      await scheduleDailyTask(whatsAppClient, user, hours, minutes);
+      await msg.reply("Daily time changed");
+    }
+  },
+});
