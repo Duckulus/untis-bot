d25281024cd96132dd35ccbf0fa6fd8e9bb8a16e
Amin <amin.haddou@frg.wwschool.de>
2023-02-06 10:57:39 +0100

    add country code selector to phone number input

--- apps/web/src/pages/register.tsx
+++ apps/web/src/pages/register.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NextPage } from "next";
 import { Field, Form, Formik, FormikHelpers } from "formik";
 import { useRouter } from "next/router";
 import { BACKEND_URL } from "@untis-bot/env";
 
 interface UserData {
+  country: string;
   number: string;
   untis_school: string;
   untis_username: string;
@@ -12,8 +13,15 @@ interface UserData {
   untis_eap: string;
 }
 
+type CountryCode = {
+  name: string;
+  dial_code: string;
+  code: string;
+};
+
 const RegisterPage: NextPage = () => {
   const initialValues: UserData = {
+    country: "+49",
     number: "",
     untis_school: "",
     untis_username: "",
@@ -23,6 +31,15 @@ const RegisterPage: NextPage = () => {
 
   const router = useRouter();
   const [error, setError] = useState("");
+  const [countryCodes, setCountryCodes] = useState<CountryCode[]>([]);
+
+  useEffect(() => {
+    fetch("/CountryCodes.json").then((resp) => {
+      resp.json().then((data) => {
+        setCountryCodes(data);
+      });
+    });
+  }, []);
 
   const handleSubmit = async (
     values: UserData,
@@ -30,7 +47,14 @@ const RegisterPage: NextPage = () => {
   ) => {
     const res = await fetch(`${BACKEND_URL}/verify`, {
       method: "post",
-      body: JSON.stringify(values),
+      body: JSON.stringify({
+        ...values,
+        number:
+          values.country +
+          (values.number.startsWith("0")
+            ? values.number.substring(1)
+            : values.number),
+      }),
       headers: {
         "Content-Type": "application/json",
       },
@@ -53,7 +77,16 @@ const RegisterPage: NextPage = () => {
             <div>
               <label>Number</label>
               <br />
-              <Field type="tel" name="number" pattern="\+[0-9]{13}" />
+              <Field as="select" name="country">
+                {countryCodes.map((code, i) => {
+                  return (
+                    <option key={i} value={code.dial_code}>
+                      {code.code} {code.dial_code}
+                    </option>
+                  );
+                })}
+              </Field>
+              <Field type="tel" name="number" pattern="^[0-9]*$" />
             </div>
             <div>
               <label>School</label>
